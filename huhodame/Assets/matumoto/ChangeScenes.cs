d4aaf51afce4a6d0aee397e4f6f8d675b09e2fee
m-taku <kbc17b19@stu.kawahara.ac.jp>
2019-10-09 16:00:48 +0900

    シーン切り替え

---
+++ huhodame/Assets/matumoto/ChangeScenes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes : MonoBehaviour
+{
+    bool m_scene = false;
+    [SerializeField] int No;
+    private void Update()
+    {
+        if (m_scene)
+        {
+            Change_Scenes(No);
+        }
+    }
+    public void Change()
+    {
+        m_scene = true;
+    }
+    void Change_Scenes(int No)
+    {
+        SceneManager.LoadScene(No);
+    }
+}
