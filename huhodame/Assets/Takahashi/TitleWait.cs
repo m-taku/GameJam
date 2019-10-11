db15766e2f885711d89d3b80f51ed129dc1dcf6d
MYU <kbc18b35@stu.kawahara.ac.jp>
2019-10-11 13:25:51 +0900

    チームロゴの表示

---
+++ huhodame/Assets/Takahashi/TitleWait.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleWait : MonoBehaviour
+{
+    public float timeOut; //時間のボーダー
+    private float timeElapsed;
+
+    GameObject Logo = null;
+    int Timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Logo = GameObject.Find("Logo");
+        Vector4 col = Logo.GetComponent<Image>().color;
+        col.w = 0.0f;
+        Logo.GetComponent<Image>().color = col;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed >= timeOut)
+        {
+            // Do anything
+            if(Timer > 30 && Timer < 60)
+            {
+                Vector4 col = Logo.GetComponent<Image>().color;
+                col.w += 0.05f;
+                if (col.w > 1.0f)
+                {
+                    col.w = 1.0f;
+                }
+                Logo.GetComponent<Image>().color = col;
+
+            }
+            else if(Timer>150 && Timer < 180)
+            {
+                Vector4 col = Logo.GetComponent<Image>().color;
+                col.w -= 0.05f;
+                if (col.w < 0.0f)
+                {
+                    col.w = 0.0f;
+                }
+                Logo.GetComponent<Image>().color = col;
+            }
+
+            Timer++;
+
+            timeElapsed = 0.0f;
+        }
+
+    }
+}
