89e7e7db6a87175a0a7890e79dca2d9157dd34b5
Yuuki Ishikawa <kbc18b01@stu.kawahara.ac.jp>
2019-10-11 15:22:07 +0900

    なぜ

--- huhodame/Assets/Takahashi/TimeLight.cs
+++ huhodame/Assets/Takahashi/TimeLight.cs
@@ -10,10 +10,12 @@ public class TimeLight : MonoBehaviour
     GameTime.day Hoge_Day = GameTime.day.Morning;
     GameObject GameObj;
     public Material night_sky = null;
-
+    public AudioSource[] sound;
+    public int num = 0;
     // Start is called before the first frame update
     void Start()
     {
+        sound = GetComponents<AudioSource>();
         GameObj = GameObject.Find("GameObject");
     }
 
@@ -21,7 +23,7 @@ public class TimeLight : MonoBehaviour
     void Update()
     {
         timeElapsed += Time.deltaTime;
-
+        sound[num].Play();
         if (timeElapsed >= timeOut)
         {
             // Do anything
@@ -47,10 +49,10 @@ public class TimeLight : MonoBehaviour
         {
             case GameTime.day.Morning:
                 light.color = new Vector4(0.6985582f, 0.7487539f, 0.8867924f, 1.0f);
-
+                num = 0;
                 break;
             case GameTime.day.Noon:
-
+                num = 1;
                 if (Timer < 60)
                 {
                     //light.color = new Vector4(1.0f, 0.8649839f, 0.8649839f, 1.0f);
@@ -63,6 +65,7 @@ public class TimeLight : MonoBehaviour
 
                 break;
             case GameTime.day.Night:
+                num = 2;
                 //light.color = new Vector4(0.0773407f, 0.1126049f, 0.2075472f, 1.0f);
                 if (Timer == 5)
                 {
